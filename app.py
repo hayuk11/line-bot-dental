e85b06ca9c1dda07c4cc3e90f7ae0b8f9e4ed86a
hayuk11 <hyfv309@hotmail.com>
2025-04-26 13:10:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,3 @@ def callback():
             send_message(reply_token, reply_text)
 
     return "OK"
-user_id = event["source"]["userId"]
-user_message = event["message"]["text"]
-reply_token = event["replyToken"]
-
-# --- Novo código para mostrar o userId
-if user_message.lower() == "meuid":
-    send_line_message(reply_token, [{
-        "type": "text",
-        "text": f"Seu userId é: {user_id}"
-    }])
-    return "OK"
-# --- Fim do novo código
